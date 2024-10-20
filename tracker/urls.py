e8ba79398e83eb3c0714fa7c6dce7d37f4f7c44b
Sachin <sachnaror@gmail.com>
2024-10-20 15:16:50 +0530

    Each day offers a lesson in perseverance

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import (generate_link,  # Ensure these views are defined
-                    track_user, view_tracked_data)
+from .views import generate_link  # Ensure these views are defined
+from .views import track_user, view_tracked_data
 
 urlpatterns = [
-    path('generate_link/', generate_link, name='generate_link'),  # Link to generate tracking link
-    path('track_user/<uuid:unique_id>/', track_user, name='track_user'),  # Track user with unique ID
-    path('view_tracked_data/<uuid:unique_id>/', view_tracked_data, name='view_tracked_data'),  # View tracking data
+    path('', generate_link, name='generate_link'),  # Link to generate tracking link
+    path('t/<uuid:unique_id>/', track_user, name='track_user'),  # Track user with unique ID
+    path('v/<uuid:unique_id>/', view_tracked_data, name='view_tracked_data'),  # View tracking data
 ]
